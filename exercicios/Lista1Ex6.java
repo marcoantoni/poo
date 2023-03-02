09ca7085b0f5b49368cedac489afb3d78960e3a0
Marco Antoni <marco.antoni910@gmail.com>
2023-03-01 21:34:43 -0300

    Correção utilizando vetor

--- exercicios/Lista1Ex6.java
+++ exercicios/Lista1Ex6.java
@@ -2,48 +2,12 @@ public class Lista1Ex6{
 
 	public static void main (String args[]){
 		int dia = 17, mes = 4, ano = 2023;
+				
+		// código com 36 linhas usando switch
 		
-		String msg = dia + " de ";
+		String meses[] = {"", "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
 		
-		switch (mes) {
-			case 1: 
-				msg += "janeiro";
-				break;
-			case 2: 
-				msg += "fevereiro";
-				break;
-			case 3:
-				msg += "março";
-				break;
-			case 4:
-				msg += "abril";
-				break;
-			case 5:
-				msg += "maio";
-				break;
-			case 6:
-				msg += "junho";
-				break;
-			case 7:
-				msg += "julho";
-				break;
-			case 8:
-				msg += "agosto";
-				break;
-			case 9:
-				msg += "setembro";
-				break;
-			case 10:
-				msg += "outubro";
-				break;
-			case 11:
-				msg += "novembro";
-				break;
-			default:
-				msg += "dezembro";
-		}
-		
-		msg += " de " + ano;
+		String msg = dia + " de " + meses[mes]  +" de " + ano;
 		
 		System.out.println(msg);
 	}
