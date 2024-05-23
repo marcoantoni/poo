a44c0072acbb4b5f3158e4e5f9484b713245a52c
Marco Antoni <marco.antoni910@gmail.com>
2024-05-22 21:55:25 -0300

    Exemplo de métodos estáticos da classe Math

---
+++ ExemploStatic.java
@@ -0,0 +1,21 @@
+class ExemploStatic {
+
+
+	public static void main (String args[]){
+
+		// exemplos de métodos estáticos da classe Math
+		
+		System.out.printf("A raiz quadrada de 16 é %f \n", Math.sqrt(16) ); 
+		
+		System.out.printf("2³² é = %f \n", Math.pow(2, 32) );
+		
+		float maior = Math.max(80, 90.1f);
+		
+		System.out.printf("O maior valor é %f \n", maior);
+		
+		int menor = Math.min(107, 74);
+		System.out.printf("O menor valor é %d \n", menor);
+	
+	}
+
+}
