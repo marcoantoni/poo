1a10761a380bda8a0b0a00b0acc27f3721a8e24a
marco <marco.antoni910@gmail.com>
2026-02-18 21:09:06 -0300

    Primeiro código em java

---
+++ OlaMundo.java
@@ -0,0 +1,10 @@
+// o nome da classe deve coincidir com o nome do arquivo
+class OlaMundo {
+	
+	// duas barras indicam um comentário
+	
+	// declarando o método principal
+	public static void main (String args[]) {
+		System.out.println("Bem vindo ao mundo da POO");
+	}
+}
